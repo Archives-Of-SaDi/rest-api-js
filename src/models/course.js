743e408b773d55a38ce6bfb1ea822abc2e00208d
sad1go0 <sayidullohabbasov0909@gmail.com>
2022-01-25 00:31:39 +0500

    added course api

---
+++ src/models/course.js
@@ -0,0 +1,56 @@
+const { Schema, model } = require('mongoose');
+const Joi = require('joi');
+
+const courseSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 255
+  },
+  category: {
+    type: new Schema({
+      name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50
+      }
+    }),
+    required: true
+  },
+  trainer: {
+    type: String,
+    required: true
+  },
+  tags: {
+    type: [String]
+  },
+  status: {
+    type: String,
+    enum: ['Active', 'Inactive'],
+    required: true
+  },
+  fee: {
+    type: Number,
+    required: true
+  }
+})
+
+const Course = model('course', courseSchema);
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    title: Joi.string().min(3).max(50).required(),
+    categoryId: Joi.string().required(),
+    trainer: Joi.string().required(),
+    status: Joi.string().required(),
+    tags: Joi.array().items(Joi.string()),
+    fee: Joi.number().min(0)
+  })
+
+  return schema.validate(course);
+}
+
+module.exports = { Course, validateCourse };
